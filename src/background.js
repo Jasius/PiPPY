ef5cba4bf3d6af8bc64c77fa2a313019081f1f39
Mindaugas Jasiulis <jasius@outlook.com>
2019-02-03 15:13:40 +0200

    fixes #10, fixes #11, fixes #9

--- src/background.js
+++ src/background.js
@@ -1,4 +1,25 @@
 "use strict";
+const pippyJS = `
+(async () => {
+  const sourcesArray = Array.from(document.querySelectorAll('video')).filter(video => video.readyState != 0).filter(video => video.disablePictureInPicture == false).sort((v1, v2) => {
+    const v1Rect = v1.getClientRects()[0];
+    const v2Rect = v1.getClientRects()[0];
+    return ((v2Rect.width * v2Rect.height) - (v1Rect.width * v1Rect.height));
+  });
+  if (sourcesArray.length === 0)
+    return;
+  const video = sourcesArray[0];
+  if (video.hasAttribute('pippystatus')) {
+    await document.exitPictureInPicture();
+  } else {
+    await video.requestPictureInPicture();
+    video.setAttribute('pippystatus', true);
+    video.addEventListener('leavepictureinpicture', event => {
+      video.removeAttribute('pippystatus');
+    }, { once: true });
+  }
+})();
+    `;
 if (!document.pictureInPictureEnabled) {
   browserName.browserAction.setTitle({
     title: "This feature isn't supported in this version of Chrome."
@@ -9,12 +30,12 @@ if (!document.pictureInPictureEnabled) {
   });
 } else {
   browserName.browserAction.onClicked.addListener(tab => {
-    browserName.tabs.executeScript({ file: "pippy.js" });
+    browserName.tabs.executeScript({ code: pippyJS, allFrames: true });
   });
 }
 
 browserName.commands.onCommand.addListener(function(command) {
-  browserName.tabs.executeScript({ file: "pippy.js" });
+  browserName.tabs.executeScript({ code: pippyJS, allFrames: true });
 });
 
 browserName.contextMenus.create({
@@ -25,6 +46,6 @@ browserName.contextMenus.create({
 
 browserName.contextMenus.onClicked.addListener(function(info, tab) {
   if (tab) {
-    browserName.tabs.executeScript(tab.id, { file: "pippy.js" });
+    browserName.tabs.executeScript({ code: pippyJS, allFrames: true });
   }
 });
